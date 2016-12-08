bb3af057f379b8840b1a83a8562920f312f11475
soderlind <per@soderlind.no>
2016-12-08 01:29:40 +0100

    Change space to tab

--- js/customizer-toggle-control.js
+++ js/customizer-toggle-control.js
@@ -6,23 +6,23 @@
 ( function( $ ) {
 	wp.customize.bind( 'ready', function() { // Ready?
 
-	    var customize = this; // Customize object alias.
+		var customize = this; // Customize object alias.
 		// Array with the control names
 		// TODO: Replace #CONTROLNAME01#, #CONTROLNAME02# etc with the real control names.
 		var toggleControls = [
-            '#CONTROLNAME01#',
-            '#CONTROLNAME02#'
-        ];
-	    $.each( toggleControls, function( index, control_name ) {
+			'#CONTROLNAME01#',
+			'#CONTROLNAME02#'
+		];
+		$.each( toggleControls, function( index, control_name ) {
 			customize( control_name, function( value ) {
-		        var controlTitle = customize.control( control_name ).container.find( '.customize-control-title' ); // Get control  title.
+				var controlTitle = customize.control( control_name ).container.find( '.customize-control-title' ); // Get control  title.
 				// 1. On loading.
 				controlTitle.toggleClass('disabled-control-title', !value.get() );
-		        // 2. Binding to value change.
-		        value.bind( function( to ) {
-		            controlTitle.toggleClass( 'disabled-control-title', !value.get() );
-		        } );
-		    } );
+				// 2. Binding to value change.
+				value.bind( function( to ) {
+					controlTitle.toggleClass( 'disabled-control-title', !value.get() );
+				} );
+			} );
 		} );
 	} );
 } )( jQuery );
